8f452e43be7397624982b4412686dc0e02d1ceff
larry sigo <sigocode@gmail.com>
2016-07-04 02:18:06 +0100

    inital commit

---
+++ app/settings_production.py
@@ -0,0 +1,20 @@
+from app.settings import *
+import dj_database_url
+
+#settings for production 
+ALLOWWED_HOSTS =['*']
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+
+DEBUG = False
+TEMPLATE_DEBUG = True
+#for database
+DATABASES['default'] = dj_database_url.config()
+# for serving satic files in a production server
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'), )
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+#added
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
